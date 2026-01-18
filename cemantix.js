e4c331d66da3d7c75dc7ef3bfeb2950417ffc18b
niico <5653050+niicojs@users.noreply.github.com>
2026-01-18 18:51:05 +0100

    up

--- cemantix.js
+++ cemantix.js
@@ -13,18 +13,17 @@ export const client = ofetch.create({
 
 export async function getPuzzleNumber() {
   const body = await client('/', { method: 'GET' });
-  let part = body.slice(
-    body.indexOf('data-puzzle-number="') + 'data-puzzle-number="'.length
-  );
+  let part = body.slice(body.indexOf('data-puzzle-number="') + 'data-puzzle-number="'.length);
   part = part.slice(0, part.indexOf('"'));
-  console.log(part);
   return part;
 }
 
 export async function checkWord(puzzle, word) {
-  const { s, p } = await client('/score', {
+  const data = await client('/score', {
     query: { n: puzzle },
     body: 'word=' + word,
   });
+  if (data.e) return { score: 0, percentile: 0 };
+  const { s, p } = data;
   return { score: s * 100, percentile: p || 0 };
 }
